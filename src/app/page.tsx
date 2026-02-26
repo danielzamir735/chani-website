4e8d3bf9281a8b399a033d8bf13e46ab4de1588b
Daniel Zamir <daniel@example.com>
2026-02-26 19:05:17 +0200

    feat: massive design and copy overhaul - pro landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Services from "@/components/Services";
-import Process from "@/components/Process";
-import WhatsAppButton from "@/components/WhatsAppButton";
-
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <Hero />
-        <Services />
-        <Process />
-      </main>
-      <WhatsAppButton />
-    </>
+    <main />
   );
 }
